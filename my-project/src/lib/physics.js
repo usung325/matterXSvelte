fd9f717b48cb71179a7ab6b88f0462fee69cb028
usung325 <usung325@gmail.com>
2024-03-18 19:46:57 -0400

    added mouseConstraint

--- my-project/src/lib/physics.js
+++ my-project/src/lib/physics.js
@@ -8,35 +8,41 @@ var Engine = Matter.Engine,
     Composite = Matter.Composite;
 
 export function createPhysicsEngine() {
-  // Initialize Matter.js engine
-  const engine = Matter.Engine.create();
-  // Add a renderer
-  const render = Matter.Render.create({
-    element: document.body,
-    engine: engine
-  });
-///////////////////////////////////////////////////////////////////////////////////////////////
-// create two boxes and a ground
-let boxA = Bodies.rectangle(400, 200, 80, 80);
-let boxB = Bodies.rectangle(450, 50, 80, 80);
-let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    // Initialize Matter.js engine
+    const engine = Matter.Engine.create();
+    // Add a renderer
+    const render = Matter.Render.create({
+        element: document.body,
+        engine: engine
+    });
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // create two boxes and a ground
+    let boxA = Bodies.rectangle(400, 200, 80, 80);
+    let boxB = Bodies.rectangle(450, 50, 80, 80);
+    let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-// Add bodies, constraints, etc.
-let rectangle = Bodies.rectangle(200, 200, 50, 50, {
-render: {
-    fillStyle: 'blue',
-    strokeStyle: 'black'
-}
-});
-//////////////////////////////////////////////////////////////////////////////////////////
-  Matter.World.add(engine.world, [rectangle, boxB, boxA, ground]);
-  // ...
+    // Add bodies, constraints, etc.
+    let rectangle2 = Bodies.rectangle(400, 250, 100, 50);
 
-  // Run the engine
-  Matter.Runner.run(engine);
-  // Render the scene
-  Matter.Render.run(render);
+    //////////////////////////////////////////////////////////////////////////////////////////
 
-  // Return the engine or any other necessary objects
-  return engine;
+    let mouse = Matter.Mouse.create(render.canvas);
+    let mouseConstraint = Matter.MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+            render: { visible: false }
+        }
+    });
+    render.mouse = mouse;
+
+    Matter.World.add(engine.world, [rectangle2, boxB, boxA, ground, mouseConstraint]);
+    // ...
+
+    // Run the engine
+    Matter.Runner.run(engine);
+    // Render the scene
+    Matter.Render.run(render);
+
+    // Return the engine or any other necessary objects
+    return engine;
 };
